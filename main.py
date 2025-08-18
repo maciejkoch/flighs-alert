db40b648dfe4b2c9d7ad698eca97f23d2b9999de
Maciej Koch <maciejkoch90@gmail.com>
2025-08-18 22:20:26 +0200

    fast api init

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from flights-alert!")
+from fastapi import FastAPI
+import uvicorn
+from app.api import health, root
+
+app = FastAPI()
+
+app.include_router(root.router)
+app.include_router(health.router)
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
